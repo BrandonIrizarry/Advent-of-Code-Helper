5d804f3121af5f4d8390f7ad61ad6ed468698aff
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-12-03 23:21:34 -0500

    WIP: Outline how to save and restore cookie object

--- helper.el
+++ helper.el
@@ -1,5 +1,111 @@
 ;; -*- lexical-binding: t -*-
 
+(defgroup advent-of-code-helper nil
+  "A library for organizing Advent of Code solutions.
+
+In particular, this library manages a user's cookie hash for
+automating both downloading puzzle input, and uploading
+solutions."
+  :version 1.0
+  :group 'local)
+
+(defcustom aoch-top-level-directory "~/adventofcode/"
+  "The top level directory containing your Advent of Code solutions."
+  :type 'directory
+  :tag "Advent of Code Helper Top Level Directory"
+  :group 'advent-of-code-helper)
+
+(defcustom aoch-cookie-name "cookie.el"
+  "The name of the file containing your Advent of Code cookie hash.
+
+This file is stored directly under your top-level directory."
+  :type 'string
+  :set-after '(aoch-top-level-directory)
+  :tag "Advent of Code Helper Cookie Hash Filename"
+  :group 'advent-of-code-helper)
+
+(require 'eieio-custom)
+(require 'eieio-base)
+
+(defclass aoch-cookie (eieio-named eieio-persistent)
+  ((hash :initarg :hash
+         :initform ""
+         :custom string
+         :label "Cookie hash"
+         :documentation
+         "The hash used to validate HTTP requests made to the Advent of
+Code website.")))
+
+(cl-defmethod eieio-done-customizing ((cookie aoch-cookie))
+  "Override this hook to ensure that the cookie is saved to disk
+after customization."
+  (eieio-persistent-save cookie))
+
+(setq foo (aoch-cookie :object-name "Advent of Code Helper Cookie"
+                       :file (let ((name (concat aoch-top-level-directory aoch-cookie-name)))
+                               (unless (file-exists-p name)
+                                 (make-empty-file name))
+                               name)))
+
+(eieio-customize-object foo)
+
+;; This seems to be the key for using the default implementation.
+(cl-defmethod aoch-load-cookie ((cookie aoch-cookie))
+  (let ((default-directory (file-name-directory (oref cookie file))))
+    (eieio-persistent-read (eieio-persistent-path-relative foo aoch-cookie-name)
+                           aoch-cookie)))
+
+(aoch-load-cookie foo)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(unless (url-cookie-retrieve ".adventofcode.com" "/")
+  (user-error "Run advent-of-code-session-bootstrap"))
+
+(aoc-session-load)
+
+
+(cl-defun aoc-retrieve-input (&key year day)
+  "Retrieve and return puzzle input from URL named INPUT-SOURCE.
+
+If unsuccessful, return NIL."
+  (with-current-buffer (url-retrieve-synchronously (format "https://adventofcode.com/%d/day/%d/input" year day))
+    (goto-char (point-min))
+    (re-search-forward "HTTP/1.1 \\([[:digit:]]+\\)" (line-end-position))
+    (when (string= (match-string 1) "200")
+      (re-search-forward "^$" nil t)
+      (delete-region (point-min) (point))
+      (buffer-string))))
+
+;; Make a cookie
+;; Customize it
+;; Save it
+(defun aoc-session-bootstrap ()
+  "Bootstrap the AOC cookie."
+  (interactive)
+  (let ((cookie (aoc-session-cookie :object-name "Advent of Code Session Cookie"
+                                    :file aoc-session-cookie-file)))
+    (eieio-customize-object cookie)
+))
+
+(defun aoc-session-load ()
+  "Load an existing cookie.
+
+If the cookie is missing, return NIL."
+  (let ((cookie-file (concat aoc-top-level-directory
+                             aoc-session-cookie-file)))
+    (when (file-exists-p cookie-file)
+      (let ((cookie (eieio-persistent-read
+                     aoc-session-cookie)))
+        cookie))))
+
+(aoc-session-load)
+
+(aoc-retrieve-input :year 2023 :day 1)
+
+
+
+
 ;;; Commentary
 ;;;
 ;;; This is meant to use a session from a logged-in Advent of Code
@@ -73,16 +179,6 @@ If unsuccessful, return NIL."
       (delete-region (point-min) (point))
       (buffer-string))))
 
-(_ (aoc-reset-cookie "~/tmp/scratch/adventofcode/session-cookie.txt")
-   ;; Now that the cookie situation is fixed, try again.
-   (aoc-retrieve-input))
-
-(defmacro until (test &rest body)
-  "Reverse the sense of WHILE."
-  (declare (indent 1))
-  `(while (not ,test)
-     ,@body))
-
 (progn
   (when (null url-cookie-storage)
     (aoc-load-cookie "~/tmp/scratch/adventofcode/session-cookie.txt"))
@@ -109,5 +205,5 @@ of Code itself."
   (make-directory (format "~/tmp/scratch/adventofcode/%d/day/%d" year day)))
 
 ;; Local Variables:
-;; read-symbol-shorthands: (("aoc-" . "advent-of-code-"))
+;; read-symbol-shorthands: (("aoch-" . "advent-of-code-helper-"))
 ;; End:
