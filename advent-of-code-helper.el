8f1cd251ea89a1dd051c0cfe275a997ce8ff8f34
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-12-04 19:44:09 -0500

    Use a nicer menu-heading string

--- advent-of-code-helper.el
+++ advent-of-code-helper.el
@@ -160,7 +160,7 @@ The user is free to provide their own bindings for
 
 (easy-menu-define nil aoch-map
   "Menu for Advent of Code Helper."
-  '("AOCH"
+  '("Advent-of-Code"
     ["Bootstrap Cookie" aoch-bootstrap]
     ["Prepare Puzzle" aoch-prepare-puzzle]
     ["Visit Puzzle" aoch-visit-puzzle]))
